28648e13b9241e29eaa6ce0bc0e2d597b3bc263e
Karolis Jocevičius <karolis.jocevicius@gmail.com>
2019-01-14 19:36:41 +0200

    cli utils

---
+++ Spaceman.Loader/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System;
+
+namespace Spaceman.Loader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new CommandLineApplication();
+            app.Name = "ninja";
+            app.HelpOption("-?|-h|--help");
+        }
+    }
+}
